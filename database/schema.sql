67ac0e3813296b11e268ee7527d5bf020873418e
Mike Glenn <mjglenn+lf@gmail.com>
2025-02-24 14:06:22 -0800

    sql check on rating

--- database/schema.sql
+++ database/schema.sql
@@ -11,7 +11,7 @@ create table "public"."movies" (
   "title"       text           not null,
   "summary"     text           not null,
   "link"        text           not null,
-  "rating"      integer        not null,
+  "rating"      integer        not null check (rating >= 1 and rating <= 5),
   "createdAt"   timestamptz(6) not null default now(),
   "updatedAt"   timestamptz(6) not null default now(),
   primary key ("movieId")
